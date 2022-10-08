ad942c9e303034aadf2c5f64eb14799d7f130fdd
Jaraxxxus <Slvsaz2018@gmail.com>
2022-10-08 08:49:01 +0300

    theoretical solution

--- Task1/src/main/cft/shift/task1.java
+++ Task1/src/main/cft/shift/task1.java
@@ -3,21 +3,24 @@ import java.io.IOException;
 
 
 public class task1 {
+    int curNum = 1;
     int mult = 1;
     int tableSize = 0;
     int multLength = 0;
     int fieldLength = 0;
+
+
     String delimitr;
-    int curNum = 1;
     StringBuilder curBuf;
     task1()
     {
+
         run();
     }
 
 
 
-int findLength(int num)
+private int findLength(int num)
 {
     int res = 0;
     while (num!=0)
@@ -27,7 +30,7 @@ int findLength(int num)
     }
     return res;
 }
-    void run() {
+    private void run() {
         System.out.println("Введите размер поля");
         try {
             tableSize=System.in.read();
@@ -38,13 +41,14 @@ int findLength(int num)
         multLength = findLength(tableSize);
         fieldLength = findLength(tableSize*tableSize);
         createDelimiter();
-        //curBuf.append();
+        printTable();
+
 
 
 
 
     }
-    void createDelimiter()
+    private void createDelimiter()
     {
         StringBuilder buf = new StringBuilder();
         buf.append("-".repeat(Math.max(0, multLength)));
@@ -59,14 +63,18 @@ int findLength(int num)
     }
 
 
-    void editField(int i)
+    private void editField(int i)
     {
         int spaceLength = multLength - findLength(i) ;
         curBuf.append(" ".repeat(Math.max(0, spaceLength))).append(curNum);
     }
+    private void editField()
+    {
+        curBuf.append(" ".repeat(Math.max(0, multLength)));
+    }
 
 
-    void fillTable()
+   private void fillBuf()
     {
         for (int i = 0 ;i < tableSize; ++i)
         {
@@ -75,6 +83,23 @@ int findLength(int num)
             curNum+=mult;
         }
 
+    }
+    private void printTable()
+    {
+        editField();
+        fillBuf();
+        System.out.println(curBuf);
+        System.out.println(delimitr);
+
+        while(mult<tableSize)
+        {
+            curBuf = "";
+            editField(mult);
+            fillBuf();
+            mult++;
+            System.out.println(curBuf);
+            System.out.println(delimitr);
+        }
     }
 
 
