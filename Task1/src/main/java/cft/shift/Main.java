eaf63be024d8017e3ac30018d7777a480e02a94f
Jaraxxxus <Slvsaz2018@gmail.com>
2022-10-15 11:11:49 +0300

    no more class fields

--- Task1/src/main/java/cft/shift/Main.java
+++ Task1/src/main/java/cft/shift/Main.java
@@ -1,7 +1,40 @@
 package cft.shift;
 
+import java.util.Scanner;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        new task1();
+
+
+        final int maxSize = 32;
+        System.out.println("enter the field size");
+        Scanner in = new Scanner(System.in);
+        if (!in.hasNextInt())
+        {
+            System.out.println("That's not a number!");
+            in.close();
+            return;
+
+        }
+        int tableSize = in.nextInt();
+        in.close();
+        if (tableSize<1 )
+        {
+            System.out.println("negative number/zero detected");
+            return;
+        }
+
+        if (tableSize>maxSize )
+        {
+            System.out.println("your number is too big");
+            return;
+        }
+        Task1.printTable(tableSize);
+
+
     }
+
+
 }
