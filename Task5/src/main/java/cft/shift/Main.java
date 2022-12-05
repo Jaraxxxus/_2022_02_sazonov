b742d2eab7562be5bfd5d0a22688db7e244a9305
Jaraxxxus <Slvsaz2018@gmail.com>
2022-12-05 17:27:10 +0300

    moved configs to resources

--- Task5/src/main/java/cft/shift/Main.java
+++ Task5/src/main/java/cft/shift/Main.java
@@ -3,9 +3,6 @@ package cft.shift;
 public class Main {
     public static void main(String[] args) {
         MultiThreadFabric multiThreadFabric = new MultiThreadFabric();
-
-
-        //System.out.println(" E N D ");
     }
 
 }
